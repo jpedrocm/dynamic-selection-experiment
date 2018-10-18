997271d87663cea17d825f5da517356d60ae77f2
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-10-18 16:38:30 -0300

    Update code to python 3

--- code/generate_metrics.py
+++ code/generate_metrics.py
@@ -5,14 +5,14 @@ from utils import summarize_metrics_folds, pandanize_summary
 
 if __name__ == "__main__":
 
-	print "Step 1 - Loading data"
+	print("Step 1 - Loading data")
 	predictions_dict = load_predictions_data()
 
-	print "Step 2 - Started calculation"
+	print("Step 2 - Started calculation")
 	metrics_dict = generate_metrics(predictions_dict)
 	summary = summarize_metrics_folds(metrics_dict)
-	print "Step 2 - Finished calculation"
+	print("Step 2 - Finished calculation")
 
-	print "Step 3 - Storing metrics"
+	print("Step 3 - Storing metrics")
 	pd_summary = pandanize_summary(summary)
 	save_pandas_summary(pd_summary)
